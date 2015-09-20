a65562f27642c1b08bd0049cfef245e7308d81b8
Rohit Arora <rohitarora.cse@gmail.com>
2015-09-20 22:49:15 +0530

    Created the initial project from https://github.com/Robbert1/boot-stateless-auth

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# token-based-auth
+token-based-auth
+===================
+Needs Gradle 2 and JDK 7
+
+build with `gradle build`  
+run with `gradle run`
---
+++ src/main/java/com/jdriven/stateless/security/UserRepository.java
@@ -0,0 +1,20 @@
+package com.jdriven.stateless.security;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+	private Map<String, User> database = new HashMap<String, User>();
+	
+	public User findByUsername(String username) {
+		return database.get(username);
+	}
+	
+	public void save(User user) {
+		database.put(user.getUsername(), user);
+	}
+}
